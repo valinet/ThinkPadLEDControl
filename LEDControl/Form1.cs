8f5f034a874d23ad8ef12474068a4a8b27915c24
Valentin-Gabriel Radu <valentingabrielradu@gmail.com>
2016-09-11 23:49:38 +0300

    New version released, 1.6.0.5:
    * Keyboard illumination level persists, if the option to remember keyboard light level is set in the main UI of the app, after the lid is closed and then opened again, and "Do nothing" is set as the action to perform when closing the lid in Windows power plan settings (thanks to bug report by Mavi_No1)

--- LEDControl/Form1.cs
+++ LEDControl/Form1.cs
@@ -600,6 +600,8 @@ namespace LEDControl
 
             PowerManager.IsMonitorOnChanged += new EventHandler(MonitorOnChanged);
 
+            RegisterForPowerNotifications();
+
             ReadSettingsKBD();
 
 
@@ -1495,7 +1497,7 @@ namespace LEDControl
                         full = true;
                     }
             }
-            if (PowerManager.IsMonitorOn && (!checkTurnKBLightOff.Checked || !full))
+            if (PowerManager.IsMonitorOn && (!checkTurnKBLightOff.Checked || !full) && !isLidClosed)
             {
                 levels.Add(GetKeyboardLightlevel());
                 if (levels.Count > 5) levels.RemoveAt(0);
@@ -1621,5 +1623,103 @@ namespace LEDControl
         {
             lightTimer.Enabled = checkTurnKBLightOff.Checked;
         }
+
+        [DllImport(@"User32", SetLastError = true, EntryPoint = "RegisterPowerSettingNotification",
+            CallingConvention = CallingConvention.StdCall)]
+
+        private static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient, ref Guid PowerSettingGuid,
+            Int32 Flags);
+
+        internal struct POWERBROADCAST_SETTING
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            public byte Data;
+        }
+
+        Guid GUID_LIDSWITCH_STATE_CHANGE = new Guid(0xBA3E0F4D, 0xB817, 0x4094, 0xA2, 0xD1, 0xD5, 0x63, 0x79, 0xE6, 0xA0, 0xF3);
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int WM_POWERBROADCAST = 0x0218;
+        const int PBT_POWERSETTINGCHANGE = 0x8013;
+
+        private bool? _previousLidState = null;
+
+        private void RegisterForPowerNotifications()
+        {
+            IntPtr handle = this.Handle;
+            IntPtr hLIDSWITCHSTATECHANGE = RegisterPowerSettingNotification(handle,
+                 ref GUID_LIDSWITCH_STATE_CHANGE,
+                 DEVICE_NOTIFY_WINDOW_HANDLE);
+            Debug.WriteLine(hLIDSWITCHSTATECHANGE.ToString());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            bool handled = false;
+            switch (m.Msg)
+            {
+                case WM_POWERBROADCAST:
+                    OnPowerBroadcast(m.WParam, m.LParam);
+                    handled = true;
+                    break;
+                default:
+                    break;
+            }
+            if (handled)
+                DefWndProc(ref m);
+            else
+                base.WndProc(ref m);
+        }
+
+        private void OnPowerBroadcast(IntPtr wParam, IntPtr lParam)
+        {
+            if ((int)wParam == PBT_POWERSETTINGCHANGE)
+            {
+                POWERBROADCAST_SETTING ps = (POWERBROADCAST_SETTING)Marshal.PtrToStructure(lParam, typeof(POWERBROADCAST_SETTING));
+                IntPtr pData = (IntPtr)((int)lParam + Marshal.SizeOf(ps));
+                Int32 iData = (Int32)Marshal.PtrToStructure(pData, typeof(Int32));
+                if (ps.PowerSetting == GUID_LIDSWITCH_STATE_CHANGE)
+                {
+                    bool isLidOpen = ps.Data != 0;
+
+                    if (!isLidOpen == _previousLidState)
+                    {
+                        LidStatusChanged(isLidOpen);
+                    }
+
+                    _previousLidState = isLidOpen;
+                }
+            }
+        }
+
+        bool isLidClosed = false;
+
+        private void LidStatusChanged(bool isLidOpen)
+        {
+            if (isLidOpen)
+            {
+                isLidClosed = false;
+                //Do some action on lid open event
+                //Debug.WriteLine("{0}: Lid opened!", DateTime.Now);
+                if (rememberKBD.Checked)
+                {
+                    var query = (from item in levels
+                                    group item by item into g
+                                    orderby g.Count() descending
+                                    select g.Key).First();
+                    SetKeyboardLevel(query);
+                    lightTimer.Enabled = true;
+                    //SaveSettingsKBD();
+                }
+            }
+            else
+            {
+                //Do some action on lid close event
+                //Debug.WriteLine("{0}: Lid closed!", DateTime.Now);
+                isLidClosed = true;
+            }
+        }
+
+
     }
 }
